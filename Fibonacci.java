007ac969fd7b068cbe88c93644142caa4c30d5df
Julian <frank_julian@hotmail.com>
2019-01-22 00:18:57 +0100

    bla bla test2

---
+++ Fibonacci.java
@@ -0,0 +1,34 @@
+/* ************************************************************************** *\
+*  Fibonacci								Author	Jasmin Sert		14-620-579 *
+*													Damaris Meier	15-113-665 *
+*																			   *
+*                   	  Programmierung 1 HS 2016                             *
+*                          		Aufgabe 6-1-1                                  *
+\* ************************************************************************** */
+
+public class Fibonacci {
+	
+	public static long fib(int i){
+		long fib;
+		if (i==0){
+			fib = 0;
+			return fib;
+		}
+		if (i==1){
+			fib = 1;
+			return fib;
+		}
+		else{
+			fib = fib(i-1) + fib(i-2);
+			return fib;
+		}
+	}
+	
+	public static void main( String[] args ){
+		int i = 0;
+		while(i<50){
+			System.out.print(fib(i) + ", ");
+			i++;
+		}
+	}
+}
