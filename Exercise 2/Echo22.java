9cdb21ae509e2efad7c0fdb4da7d269b310474d5
Julian <frank_julian@hotmail.com>
2019-02-17 22:06:01 +0100

    test

---
+++ Exercise 2/Echo22.java
@@ -0,0 +1,30 @@
+//********************************************************************
+//  Echo.java       Author: Lewis/Loftus
+//
+//  Demonstrates the use of the nextLine method of the Scanner class
+//  to read a string from the user.
+//********************************************************************
+
+import java.util.Scanner;
+
+public class Echo
+{
+   //-----------------------------------------------------------------
+   //  Reads a character string from the user and prints it.
+   //-----------------------------------------------------------------
+   public static void main (String[] args)
+   {
+      String message;
+      Scanner scan = new Scanner (System.in);
+
+      System.out.println ("Enter a line of text:");
+
+      message = scan.nextLine();
+
+//gagi mit gagi^2
+//gagi4
+
+
+      System.out.println ("You entered: \"" + message + "\"");
+   }
+}
